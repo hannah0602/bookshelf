c713344f7e5bb8bcabed915e053f7d5280d22825
Hoang Nguyen <hoangna1204@gmail.com>
2023-09-17 19:51:17 +0200

    fix bug with button in book component

--- src/Book.js
+++ src/Book.js
@@ -17,7 +17,7 @@ const Book = ({ book, onUpdateCategory, onSelect }) => {
 
     const options = [...e.target.selectedOptions];
     const values = options.map((option) => option.value);
-  
+
     const category = values[0];
     book.category = category;
     setSelectedCategory(values);
@@ -29,26 +29,26 @@ const Book = ({ book, onUpdateCategory, onSelect }) => {
       <p>{book.title}</p>
       <p>{book.author}</p>
       <p>{book.category}</p>
-      <button
-        className="rounded-full w-16 hh-16 bg-blue-700 shadow-sm"
-        onClick={showMenu}
-      >
-        Select
-      </button>
+
+      {onUpdateCategory && (
+        <button
+          className="rounded-full w-16 hh-16 bg-blue-700 shadow-sm"
+          onClick={showMenu}
+        >
+          Select
+        </button>
+      )}
 
       {menu && (
-          <label>
-            Move on to...
-            <select
-              value={selectedCategory}
-              onChange={handleSubmit}
-            >
-              <option value="Currently Reading">Currently Reading</option>
-              <option value="Want To Read">Want To Read</option>
-              <option value="Read">Read</option>
-              <option value="None">None</option>
-            </select>
-          </label>
+        <label>
+          Move on to...
+          <select value={selectedCategory} onChange={handleSubmit}>
+            <option value="Currently Reading">Currently Reading</option>
+            <option value="Want To Read">Want To Read</option>
+            <option value="Read">Read</option>
+            <option value="None">None</option>
+          </select>
+        </label>
       )}
     </div>
   );
